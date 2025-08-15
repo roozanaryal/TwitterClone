cd578858e0a81b334787fa18e952e0899313e557
Aryal <roozanaryal8@gmail.com>
2025-08-15 21:10:01 +0545

    comment apart added in route

--- backend/routes/tweet.route.js
+++ backend/routes/tweet.route.js
@@ -6,6 +6,10 @@ import {
   getPosts,
   likePost,
   unlikePost,
+  updatePost,
+  deletePost,
+  addComment,
+  deleteComment,
 } from "../controller/tweet.controller.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
@@ -15,10 +19,13 @@ router.post("/createpost", authMiddleware, createTweet);
 router.get("/getposts", authMiddleware, getPosts);
 router.get("/followingfeed", authMiddleware, getFollowingFeed);
 router.get("/myposts", authMiddleware, getMyPosts);
+
 router.post("/like/:id", authMiddleware, likePost);
 router.post("/unlike/:id", authMiddleware, unlikePost);
 router.post("/update/:id", authMiddleware, updatePost);
+
 router.post("/delete/:id", authMiddleware, deletePost);
 router.post("/comment/:id", authMiddleware, addComment);
 router.post("/deletecomment/:id", authMiddleware, deleteComment);
+
 export default router;
